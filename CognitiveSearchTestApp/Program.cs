8f447a9811cc11da26a356ecd2b92dd0bfc55745
Kazuki Ota <kaota@microsoft.com>
2023-08-15 19:53:55 +0900

    プロジェクト ファイルを追加します。

---
+++ CognitiveSearchTestApp/Program.cs
@@ -0,0 +1,179 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Indexes.Models;
+using Azure.Search.Documents.Models;
+using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography;
+using System.Text.Json;
+
+// モデルのベクトルの次元数
+const int ModelDimensions = 1536;
+// インデックス名
+const string IndexName = "test-index";
+
+// 設定の読み込み
+var c = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+var openAIOptions = c.GetSection(OpenAIOptions.KeyName).Get<OpenAIOptions>() ?? throw new InvalidOperationException();
+var cognitiveSearchOptions = c.GetSection(CognitiveSearchOptions.KeyName).Get<CognitiveSearchOptions>() ?? throw new InvalidOperationException();
+
+// AOAI と Cognitive Search への接続用クライアントの作成
+var openAIClient = new OpenAIClient(
+    new Uri(openAIOptions.Endpoint),
+    new AzureKeyCredential(openAIOptions.ApiKey));
+var cognitiveSearchClient = new SearchIndexClient(
+    new Uri(cognitiveSearchOptions.Endpoint),
+    new AzureKeyCredential(cognitiveSearchOptions.ApiKey));
+
+// インデックスを作成する
+await CreateIndexAsync(cognitiveSearchClient);
+// データを登録する
+await InsertDataAsync(openAIClient, openAIOptions.ModelName, cognitiveSearchClient);
+// 検索をする
+await ReadWordAndSearchAsync(openAIClient, openAIOptions.ModelName, cognitiveSearchOptions);
+
+// インデックスを作成する
+async ValueTask CreateIndexAsync(SearchIndexClient client)
+{
+    // ベクトル検索の設定名
+    const string VectorSearchConfigName = "vector-config-for-test";
+
+    var searchIndex = new SearchIndex(IndexName)
+    {
+        VectorSearch = new()
+        {
+            AlgorithmConfigurations =
+            {
+                new HnswVectorSearchAlgorithmConfiguration(VectorSearchConfigName),
+            }
+        },
+        Fields =
+        {
+            // 一意識別のための列
+            new SimpleField("id", SearchFieldDataType.String) { IsKey = true },
+            // サービス名と概要
+            new SimpleField("serviceName", SearchFieldDataType.String),
+            // SearchableField が 1 つはないと検索できないので使わないけど SearchableField にしておく
+            new SearchableField("description") { IsFilterable = true }, 
+            // 概要のベクトルデータ
+            new SearchField("descriptionVector", SearchFieldDataType.Collection(SearchFieldDataType.Single))
+            {
+                IsSearchable = true,
+                VectorSearchDimensions = ModelDimensions,
+                VectorSearchConfiguration = VectorSearchConfigName
+            }
+        }
+    };
+
+    // 作成または更新をする
+    await client.CreateOrUpdateIndexAsync(searchIndex);
+}
+
+// ベクトル化
+async ValueTask<float[]> GenerateEmbeddingsAsync(OpenAIClient openAIClient, string modelName, string text)
+{
+    var result = await openAIClient.GetEmbeddingsAsync(modelName, new EmbeddingsOptions(text));
+    return result.Value.Data[0].Embedding.ToArray();
+}
+
+// データを作成したインデックスに追加する
+async ValueTask InsertDataAsync(OpenAIClient openAIClient, string modelName, SearchIndexClient cognitiveSearchClient)
+{
+    static async ValueTask<ServiceDescription[]?> readAsync()
+    {
+        using var file = File.OpenRead("services.json");
+        return await JsonSerializer.DeserializeAsync<ServiceDescription[]>(file, new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        });
+    }
+
+    var serviceDescriptions = await readAsync() ?? throw new InvalidOperationException();
+    List<SearchDocument> documents = new();
+    foreach (var serviceDescription in serviceDescriptions)
+    {
+        var embeddings = await GenerateEmbeddingsAsync(openAIClient, modelName, serviceDescription.Description);
+        documents.Add(new SearchDocument
+        {
+            ["id"] = serviceDescription.ServiceName.Replace(" ", ""),
+            ["serviceName"] = serviceDescription.ServiceName,
+            ["description"] = serviceDescription.Description,
+            ["descriptionVector"] = embeddings,
+        });
+    }
+
+    var searchClient = cognitiveSearchClient.GetSearchClient(IndexName);
+    await searchClient.MergeOrUploadDocumentsAsync(documents);
+}
+
+// ベクトル検索
+async ValueTask ReadWordAndSearchAsync(OpenAIClient openAIClient, string modelName, CognitiveSearchOptions options)
+{
+    // SearchIndexClient から SearchClient を作成する以外にも、直接 SearchClient を作る方法もあるのでそっちで作る
+    // 普通は検索アプリは検索だけすることが多いと思うので、その場合はこのように SearchClient を作ることになる。
+    var searchClient = new SearchClient(
+        new Uri(options.Endpoint), IndexName, new AzureKeyCredential(options.ApiKey));
+
+    while (true)
+    {
+        Console.Write("やりたいことを入力してください: ");
+        var input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input) || input == "exit")
+        {
+            break;
+        }
+
+        var embeddings = await GenerateEmbeddingsAsync(openAIClient, modelName, input);
+
+        var searchOptions = new SearchOptions
+        {
+            Vectors =
+            {
+                new SearchQueryVector
+                {
+                    KNearestNeighborsCount = 3, // 3 件返す
+                    Fields = { "descriptionVector" },
+                    Value = embeddings
+                }
+            },
+            Size = 3, // 3 件返す
+            Select = { "serviceName", "description" },
+        };
+
+        var response = await searchClient.SearchAsync<SearchDocument>(null, searchOptions);
+        Console.WriteLine("やりたいことを実現できる可能性のあるサービスは以下のものになります。");
+        await foreach (var result in response.Value.GetResultsAsync())
+        {
+            Console.WriteLine($"サービス名: {result.Document["serviceName"]}, スコア: {result.Score}");
+            Console.WriteLine($"概要: {result.Document["description"]}");
+            Console.WriteLine();
+        }
+    }
+}
+
+
+// 設定を読み込むためのクラス
+class OpenAIOptions
+{
+    public const string KeyName = "OpenAI";
+    public required string Endpoint { get; set; }
+    public required string ApiKey { get; set; }
+    public required string ModelName { get; set; }
+}
+
+class CognitiveSearchOptions
+{
+    public const string KeyName = "CognitiveSearch";
+
+    public required string Endpoint { get; set; }
+    public required string ApiKey { get; set; }
+}
+
+class ServiceDescription
+{
+    public string ServiceName { get; set; } = "";
+    public string Description { get; set; } = "";
+}
---
+++ TestDataGenerator/Program.cs
@@ -0,0 +1,96 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.Configuration;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+
+var c = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var options = c.GetSection("OpenAI")
+    .Get<OpenAIOptions>()
+    ?? throw new InvalidOperationException();
+
+var client = new OpenAIClient(
+    new Uri(options.Endpoint),
+    new AzureKeyCredential(options.ApiKey));
+
+var serviceNames = new[]
+{
+    "CosmosDB", "Azure Functions", "App Service", "SQL Database",
+    "Azure Kubernetes Service", "Azure Container Instances", "Azure Container Registry",
+    "Azure DevOps", "Azure Pipelines", "Azure Boards", "Azure Repos", "Azure Artifacts",
+    "Azure Test Plans", "Azure Monitor", "Azure Application Insights", "Azure Log Analytics",
+    "Azure Resource Manager", "Azure Resource Graph", "Azure Policy", "Azure Blueprints", "Azure Cost Management",
+    "Azure Advisor", "Azure Security Center", "Azure Sentinel", "Azure Defender", "Azure Key Vault",
+};
+
+const string systemPrompt = """
+    あなたはMicrosoft Azureの専門家として、ユーザーが指定したAzureサービスの概要を100文字以上、500文字以内で解説してください。
+    """;
+
+var results = new List<ServiceDescription>();
+foreach (var serviceName in serviceNames)
+{
+    Console.WriteLine($"サービス名: {serviceName}");
+    var chatCompletionsOptions = new ChatCompletionsOptions
+    {
+        ChoiceCount = 1,
+        MaxTokens = 1000,
+        Temperature = 0.0f,
+        Messages =
+        {
+            new ChatMessage(ChatRole.System, systemPrompt),
+            new ChatMessage(ChatRole.User, serviceName),
+        },
+    };
+
+    var response = await client.GetChatCompletionsAsync(
+        options.ModelName,
+        chatCompletionsOptions);
+
+    var choice = response.Value.Choices[0];
+    if (choice.FinishReason != CompletionsFinishReason.Stopped)
+    {
+        Console.WriteLine($"何かエラー: {choice.FinishReason}");
+        return;
+    }
+
+    results.Add(new()
+    {
+        ServiceName = serviceName,
+        Description = choice.Message.Content,
+    });
+}
+
+if (File.Exists("services.json"))
+{
+    File.Delete("services.json");
+}
+
+using var file = File.OpenWrite("services.json");
+await JsonSerializer.SerializeAsync<List<ServiceDescription>>(
+    file,
+    results,
+    new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+    });
+await file.FlushAsync();
+
+class OpenAIOptions
+{
+    public required string Endpoint { get; set; }
+    public required string ApiKey { get; set; }
+    public required string ModelName { get; set; }
+}
+
+class ServiceDescription
+{
+    public string ServiceName { get; set; } = "";
+    public string Description { get; set; } = "";
+}
